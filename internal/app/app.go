b4019252dd22de6c66375b951563430d21161150
Manuel Aguilar <manuelmla.aguilar@gmail.com>
2024-10-14 22:56:49 -0300

    Added log config output from .env

--- internal/app/app.go
+++ internal/app/app.go
@@ -2,10 +2,13 @@ package app
 
 import (
 	"database/sql"
+	"io"
 	"log"
+	"os"
 
 	"github.com/Manuel1Aguilar/portf_mger/internal/db"
 	"github.com/Manuel1Aguilar/portf_mger/internal/services"
+	"github.com/joho/godotenv"
 )
 
 type App struct {
@@ -16,6 +19,7 @@ type App struct {
 }
 
 func NewApp() (*App, error) {
+	configureLogging()
 	// Initialize the database
 	database, err := db.InitializeSQLite("portfolio.db")
 	if err != nil {
@@ -43,4 +47,27 @@ func NewApp() (*App, error) {
 func (a *App) Close() {
 	log.Println("Closing database connection...")
 	a.DB.Close()
+}
+
+func configureLogging() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	logOutput := os.Getenv("LOG_OUTPUT")
+	switch logOutput {
+	case "discard":
+		log.SetOutput(io.Discard)
+	case "stdout":
+		log.SetOutput(os.Stdout)
+	case "file":
+		file, err := os.OpenFile("app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+		if err != nil {
+			log.Fatalf("Failed to open log file: %v", err)
+		}
+		log.SetOutput(file)
+	default:
+		log.SetOutput(os.Stdout)
+	}
 }
